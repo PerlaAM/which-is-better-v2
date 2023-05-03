e1b450744ffee063a0a348f5858882ca2782eec7
Perla Aguilar <perlaelizabeth_am@hotmail.com>
2023-05-03 13:42:58 -0600

    Styles

--- src/components/ErrorValidation.tsx
+++ src/components/ErrorValidation.tsx
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+
 export default function ErrorValidation({ message }: any) {
   const MessageText = () => {
     if (message) return message;
   };
 
   return (
-    <p className={`m-0 ${message ? '' : 'd-none'}`}>
+    <p
+      className={`m-0 text-danger fs-8 fw-light pt-1 ${
+        message ? '' : 'd-none'
+      }`}
+    >
+      <FontAwesomeIcon icon={faCircleExclamation} className='me-1' />
       <MessageText />
     </p>
   );
