a5fc1eac41cd5de1fda815a97920ff970e581e02
Perla Aguilar <perlaelizabeth_am@hotmail.com>
2023-05-03 11:42:02 -0600

    Limit unit of measure & sort shopping cart by store name

--- src/data/unitMeasures.tsx
+++ src/data/unitMeasures.tsx
@@ -1,9 +1,9 @@
 import { IUnitMeasure } from '../interfaces/unitMeasureInterface';
 
 export const unitMeasureOptions: readonly IUnitMeasure[] = [
-  { value: 'g', label: 'g' },
-  { value: 'kg', label: 'kg' },
-  { value: 'ml', label: 'ml' },
-  { value: 'l', label: 'l' },
-  { value: 'pieces', label: 'pieces' },
+  { value: 'g', label: 'g', type: 'weight' },
+  { value: 'kg', label: 'kg', type: 'weight' },
+  { value: 'ml', label: 'ml', type: 'volume' },
+  { value: 'l', label: 'l', type: 'volume' },
+  { value: 'pieces', label: 'Pieces', type: 'unity' },
 ];
